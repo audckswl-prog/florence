d528f3554c42f6ab0a25f2d482982fa147f91240
Ji Myeongchan <audckswl@gmail.com>
2026-03-25 20:32:36 +0900

    Fix: Add missing supabase import in MiniTicket

--- lib/features/social/widgets/mini_completed_ticket_widget.dart
+++ lib/features/social/widgets/mini_completed_ticket_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../social/providers/social_providers.dart';
 
